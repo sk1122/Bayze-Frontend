1536b61b2d1677371c253551838ee3d65709866e
Satyam Kulkarni <punekar.satyam@gmail.com>
2021-10-09 20:52:58 +0530

    Changed Images

--- src/components/Founders.tsx
+++ src/components/Founders.tsx
@@ -1,4 +1,6 @@
 import satyam from "../images/Satyam Kulkarni.jpeg"
+import avatar from "../images/avatar.png"
+
 export default function Founders() {
 	return (
 		<div className="h-auto w-full flex flex-col justify-start items-center bg-brand-blue">
@@ -13,7 +15,7 @@ export default function Founders() {
 					<div className="text-sm font-normal text-inter mt-3">Agencies should have focus more on Designing Campaigns rather than doing basic tasks!</div>
 				</div>
 				<div className="flex flex-col justify-start items-center text-center w-72 h-48">
-					<img src="https://scontent.fpnq7-3.fna.fbcdn.net/v/t31.18172-8/15731752_830162207137172_5879917434292150775_o.jpg?_nc_cat=104&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=5J5QUyQTgsMAX_w5ZS8&_nc_ht=scontent.fpnq7-3.fna&oh=e02c8b0aae20ccde4408dedff26ad227&oe=617DCCDB" alt="" className="p-1 h-24 w-24 rounded-full" />
+					<img src={avatar} alt="" className="p-1 h-24 w-24 rounded-full" />
 					<div className="text-lg font-semibold text-inter mt-5">Prajwal Marennavar</div>
 					<div className="text-sm font-normal text-inter mt-3">Time is Money and We are here to save time of you!</div>
 				</div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -17,23 +17,27 @@ function Navbar() {
 				<img src={logo} alt="" />
 			</div>
 			<div className="h-4 w-96 hidden text-xs mr-4 md:flex justify-around items-center text-montserrat">
-				<div><Link to="features" spy={true} smooth={true} duration={500}>Features</Link></div>
-				<div><Link to="problems" spy={true} smooth={true} duration={500}>Problems</Link></div>
-				<div><a href="https://bayze.hashnode.dev/" target="_blank" rel="noreferrer">Blog</a></div>
-				<div><Link to="contact" spy={true} smooth={true} duration={500}>Contact</Link></div>
-				</div>
+				<div className="cursor-pointer"><Link to="features" spy={true} smooth={true} duration={500}>Features</Link></div>
+				<div className="cursor-pointer"><Link to="problems" spy={true} smooth={true} duration={500}>Problems</Link></div>
+				<div className="cursor-pointer"><a href="https://blog.bayze.in/" target="_blank" rel="noreferrer">Blog</a></div>
+				<div className="cursor-pointer"><Link to="contact" spy={true} smooth={true} duration={500}>Contact</Link></div>
+			</div>
 			<div className="md:hidden">
-				<button className="md:hidden absolute right-16" type="button" onClick={openMenu}>
+				<button className="md:hidden absolute right-10" type="button" onClick={openMenu}>
 					<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
 					</svg>
 				</button>
 			</div>
 			<div className="hidden text-xs md:hidden justify-around items-center text-white text-montserrat bg-brand-400 p-4 rounded-lg absolute top-24 right-10" ref={menu as React.RefObject<HTMLDivElement>}>
-				<div className="px-2 py-4">About</div>
+				<div className="cursor-pointer px-2 py-4 hover:opacity-50"><Link to="features" spy={true} smooth={true} duration={500}>Features</Link></div>
+				<div className="cursor-pointer px-2 py-4 hover:opacity-50"><Link to="problems" spy={true} smooth={true} duration={500}>Problems</Link></div>
+				<div className="cursor-pointer px-2 py-4 hover:opacity-50"><a href="https://blog.bayze.in/" target="_blank" rel="noreferrer">Blog</a></div>
+				<div className="cursor-pointer px-2 py-4 hover:opacity-50"><Link to="contact" spy={true} smooth={true} duration={500}>Contact</Link></div>
+				{/* <div className="px-2 py-4">About</div>
 				<div className="px-2 py-4">Products</div>
 				<div className="px-2 py-4"><a href="https://bayze.hashnode.dev/" target="_blank" rel="noreferrer">Blog</a></div>
-				<div className="px-2 py-4">Contact Us</div>
+				<div className="px-2 py-4">Contact Us</div> */}
 			</div>
 		</div>
 	)
