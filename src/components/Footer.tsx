31af2026b678450af885460d9bd8fcd79f628633
Satyam Kulkarni <punekar.satyam@gmail.com>
2021-10-09 21:01:22 +0530

    Changed EMail in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
 		<div id="contact" className="p-10 font-bold h-auto w-full flex flex-col justify-start items-start bg-brand-blue">
 			<img src={bayze} alt="Bayze Logo" className="w-36" />
 			<br />
-			<div className="text-inter">Email: <a href="mailto:bayzecorp@gmail.com">bayzecorp@gmail.com</a></div>
+			<div className="text-inter">Email: <a href="mailto:hello@bayze.in">hello@bayze.in</a></div>
 			<br />
 			© 2021 All rights reserved. 
 		</div>
