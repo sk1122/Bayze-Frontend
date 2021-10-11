5b8ad37dc61e19d71f4ced49723d1b8f715fd3a7
Satyam Kulkarni <punekar.satyam@gmail.com>
2021-10-11 17:16:24 +0530

    Funcked Up Changes

--- src/api/db.js
+++ src/api/db.js
@@ -13,7 +13,7 @@ export const db = async (full_name, email, company)  => {
 	await doc.loadInfo();
 	await doc.updateProperties({ title: 'Waitlist Form' })
 
-	const sheet = await doc.sheetsByIndex[1]
+	const sheet = await doc.sheetsByIndex[0]
 	
 	sheet.addRow({ full_name: full_name, email: email, company: company })
 }
